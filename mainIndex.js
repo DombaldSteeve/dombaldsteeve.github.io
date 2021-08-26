5848bdb2d1a7149c8c3a640baafa3cfe0d040975
DOMBALD Steeve <steeve.dombald.pro@outlook.com>
2021-08-26 07:57:06 +0200

    animation title

--- mainIndex.js
+++ mainIndex.js
@@ -114,3 +114,32 @@ document.querySelectorAll('[class*="reveal-"]').forEach(function (r) {
 
 
 
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = ("");
+
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>"+ splitText[i] + "</span>";
+}
+
+
+let char = 0; 
+let timer = setInterval(onTick, 50);
+
+            function  onTick(){
+            const span = text.querySelectorAll('span') [char];
+            
+            if (char === splitText.length){
+                clearInterval(timer)
+            }else{
+                char++
+                span.classList.add('fade');
+            }
+        }
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
