72c63db17089d98f4dc5ef218d06fc6e524ac0bc
DOMBALD Steeve <steeve.dombald.pro@outlook.com>
2021-08-27 23:46:26 +0200

    new title and add pics

--- mainIndex.js
+++ mainIndex.js
@@ -125,7 +125,7 @@ for (let i = 0; i < splitText.length; i++) {
 
 
 let char = 0; 
-let timer = setInterval(onTick, 50);
+let timer = setInterval(onTick, 90);
 
             function  onTick(){
             const span = text.querySelectorAll('span') [char];
